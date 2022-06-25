17fe11fe7358596b32a5d5b8b00f9de9d8af573d
chang wong <changcwong@gmail.com>
2022-06-25 15:32:48 -0700

    Initializing

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 
 // Select some elements...
 
+let header = document.querySelector('#page-header')
+header.getElementsByClassName.textAlign = "left"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i=0; i<dogImages.length;i++) {
+dogimages[i].style.borderRadius = "50px"
+}
